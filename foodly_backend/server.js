119fbc7513926c3694466ce38a166217efff25e3
channoufi-saber <saber.channoufi@esprit.tn>
2024-05-28 21:16:02 +0100

    Restuarant schema and route

--- foodly_backend/server.js
+++ foodly_backend/server.js
@@ -1,25 +1,29 @@
-const express=require('express')
-const dotenv=require('dotenv')
-const mongoose=require('mongoose')
-const bodyParser=require('body-parser')
-const app=express()
-const port=6002
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 6002
 
-const authRouter=require('./routes/auth')
-const userRouter=require('./routes/user')
+const authRouter = require('./routes/auth')
+const userRouter = require('./routes/user')
+const restaurantRouter = require("./routes/restaurant");
 
 dotenv.config()
 
-const admin=require('firebase-admin');
-const serviceAccount=require('./servicesAccountKey.json')
+const admin = require('firebase-admin');
+const serviceAccount = require('./servicesAccountKey.json')
 
-mongoose.connect(process.env.MONGO_URL).then(()=>console.log('DB connected')).catch((err)=>console.log(err))
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('DB connected')).catch((err) => console.log(err))
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/',authRouter)
-app.use('/api/users',userRouter)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use('/', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/restaurants', restaurantRouter)
 
 
-app.get('/',(req,res)=>res.send('Hello World'))
-app.listen(process.env.PORT || port,()=>console.log(`Example app listening on port ${process.env.PORT}! `))
+
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}! `))
